c6595bb7f0487390506d9ae9c5a4489eb0c3d965
Mitchell Dokken <mitchell.dokken@gmail.com>
2016-06-13 15:02:14 -0500

    quick integration
    added declareALL.pd generator so you don't have to manually do it, puts it in ~/declareALL.pd for easy calling on your pd startup

--- software/get_externals.sh
+++ software/get_externals.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 #use as get_externals.sh TWITTERACCOUNT USERNAME PASSWORD
 #account is public but i dont wanna share it
-mkdir pd-externals && cd pd-externals
+mkdir pd-externals
+cd pd-externals
 wget -O temphtml  https://twitter.com/$1  &&  grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' temphtml > ips && rm temphtml
 read -r ipaddress < ips
 wget ftp://$2:$3@$ipaddress/share/pd/externalsARMV7.zip
@@ -10,3 +11,16 @@ rm externalsARMV7.zip
 rm ips
 echo "congrats, you got the externals"
 ##combine this with the script to generate the declcareALL.pd sketch to correctly name all the stdpath startup options
+
+USER="$( logname )"
+ls > allexternals
+filename="allexternals"
+echo "#N canvas 5 57 450 294 10;" > declareALL.pd #all pd sketches start with this 
+while read -r line
+do
+    name="$line"
+    echo "#X declare -stdpath /home/$USER/pd-externals/$name;" >> declareALL.pd
+    echo "#X obj 7 34 54 declare -stdpath /home/$USER/pd-externals/$name;" >> declareALL.pd
+done < "$filename"
+mv declareALL.pd ~/declareALL.pd
+rm allexternals
