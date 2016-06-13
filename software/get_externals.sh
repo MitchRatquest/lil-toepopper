fc4893a45330a2bf764c0ba16ad2c11df0a9c01e
Mitchell Dokken <mitchell.dokken@gmail.com>
2016-06-13 15:22:14 -0500

    Update get_externals.sh

--- software/get_externals.sh
+++ software/get_externals.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 #use as get_externals.sh TWITTERACCOUNT USERNAME PASSWORD
 #account is public but i dont wanna share it
-mkdir pd-externals
-cd pd-externals
-wget -O temphtml  https://twitter.com/$1  &&  grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' temphtml > ips && rm temphtml
-read -r ipaddress < ips
-wget ftp://$2:$3@$ipaddress/share/pd/externalsARMV7.zip
-unzip externalsARMV7.zip -d .
-rm externalsARMV7.zip
-rm ips
-echo "congrats, you got the externals"
+mkdir ~/pd-externals
+#cd pd-externals
+#wget -O temphtml  https://twitter.com/$1  &&  grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' temphtml > ips && rm temphtml
+#read -r ipaddress < ips
+#wget ftp://$2:$3@$ipaddress/share/pd/externalsARMV7.zip
+unzip externalsARMV7.zip -d ~/pd-externals
+#rm externalsARMV7.zip
+#rm ips
+#echo "congrats, you got the externals"
 ##combine this with the script to generate the declcareALL.pd sketch to correctly name all the stdpath startup options
-
+cd ~/pd-externals
 USER="$( logname )"
 ls > allexternals
 filename="allexternals"
