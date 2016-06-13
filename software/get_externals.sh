8cdba875cca3b7333547cbda9a390d8fa9b0a038
Mitchell Dokken <mitchell.dokken@gmail.com>
2016-06-13 14:53:21 -0500

    externals

--- software/get_externals.sh
+++ software/get_externals.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #use as get_externals.sh TWITTERACCOUNT USERNAME PASSWORD
 #account is public but i dont wanna share it
-mkdir externals && cd externals
+mkdir pd-externals && cd pd-externals
 wget -O temphtml  https://twitter.com/$1  &&  grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' temphtml > ips && rm temphtml
 read -r ipaddress < ips
 wget ftp://$2:$3@$ipaddress/share/pd/externalsARMV7.zip
